45749d3650ffb700715774671efc8e2f5fbd28a4
Thuhien021003 <nguyenluongthuhienqn1@gmail.com>
2024-03-21 14:34:59 +0700

    1

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,52 @@
-function startday(){
-    // Lấy các element select
+function day(){
     var startDaySelect = document.getElementById("start-day");
-    
-    // Tạo danh sách ngày từ 1 đến 31
-    for (var i = 1; i <= 31; i++) {
-        var option = document.createElement("option");
-        option.value = i;
-        option.text = i;
-        startDaySelect.appendChild(option);
-    }
-}
-function endday(){
     var endDaySelect = document.getElementById("end-day");
+if (startDaySelect.options.length === 0) {
     for (var i = 1; i <= 31; i++) {
-    var option2 = document.createElement("option");
-    option2.value = i;
-    option2.text = i;
-    endDaySelect.appendChild(option2);
-    }
-}
+      var option = document.createElement("option");
+      option.value = i;
+      option.text = i;
+      startDaySelect.appendChild(option);
 
-function startmonth(){
-    var startMonthSelect = document.getElementById("start-month");
-     // Tạo danh sách tháng từ 1 đến 12
-    for (var i = 1; i <= 12; i++) {
-        var option = document.createElement("option");
-        option.value = i;
-        option.text = i;
-        startMonthSelect.appendChild(option);
+      var option2 = document.createElement("option");
+      option2.value = i;
+      option2.text = i;
+      endDaySelect.appendChild(option2);
     }
+  }
 }
-function endmonth(){
-    var endMonthSelect = document.getElementById("end-month");
+function month(){
+var startMonthSelect = document.getElementById("start-month");
+  var endMonthSelect = document.getElementById("end-month");
+if (startMonthSelect.options.length === 0) {
     for (var i = 1; i <= 12; i++) {
-        var option2 = document.createElement("option");
-        option2.value = i;
-        option2.text = i;
-        endMonthSelect.appendChild(option2);
-    }
-}
+      var option = document.createElement("option");
+      option.value = i;
+      option.text = i;
+      startMonthSelect.appendChild(option);
 
-function startyear(){
-    var startYearSelect = document.getElementById("start-year");
-    // Tạo danh sách năm từ 2020 đến 2024 (hoặc năm hiện tại)
+      var option2 = document.createElement("option");
+      option2.value = i;
+      option2.text = i;
+      endMonthSelect.appendChild(option2);
+    }
+  }
+}
+function year(){
+  var startYearSelect = document.getElementById("start-year");
+  var endYearSelect = document.getElementById("end-year");
+  if (startYearSelect.options.length === 0) {
     var currentYear = new Date().getFullYear();
     for (var i = 2020; i <= currentYear; i++) {
-        var option = document.createElement("option");
-        option.value = i;
-        option.text = i;
-        startYearSelect.appendChild(option);
+      var option = document.createElement("option");
+      option.value = i;
+      option.text = i;
+      startYearSelect.appendChild(option);
+
+      var option2 = document.createElement("option");
+      option2.value = i;
+      option2.text = i;
+      endYearSelect.appendChild(option2);
     }
-}
-function endyear(){
-    var endYearSelect = document.getElementById("end-year");
-    var currentYear = new Date().getFullYear();
-    for (var i = 2020; i <= currentYear; i++) {
-        var option2 = document.createElement("option");
-        option2.value = i;
-        option2.text = i;
-        endYearSelect.appendChild(option2);
-    }
+  }
 }
