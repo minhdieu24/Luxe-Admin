d38627f39c44d019e736d6062c62d6c24afaa1ba
bngocdao <daobaongoc2603@gmail.com>
2024-04-08 13:18:42 +0700

    tt

---
+++ js/khuyenmai.js
@@ -0,0 +1,6 @@
+$(function() {
+    // Khởi tạo Datepicker cho input có id là "TGBatDau"
+    $("#TGBatDau").datepicker();
+    // Khởi tạo Datepicker cho input có id là "TGKetThuc"
+    $("#TGKetThuc").datepicker();
+  });
