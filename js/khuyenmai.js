cd7348c74ccef55739853cdfecb75d568d18bd1b
bngocdao <daobaongoc2603@gmail.com>
2024-04-08 21:49:55 +0700

    tt

--- js/khuyenmai.js
+++ js/khuyenmai.js
@@ -1,15 +1,4 @@
-$(document).ready(function(){
-  $('#TGBatDauIcon').click(function(){
-      $('#TGBatDauInput').datetimepicker({
-          format: 'DD/MM/YYYY' // Định dạng hiển thị thời gian
-      });
-      $('#TGBatDauInput').datetimepicker('show'); // Hiển thị datetimepicker khi icon được click
-  });
-
-  $('#TGKetThucIcon').click(function(){
-      $('#TGKetThucInput').datetimepicker({
-          format: 'DD/MM/YYYY' // Định dạng hiển thị thời gian
-      });
-      $('#TGKetThucInput').datetimepicker('show'); // Hiển thị datetimepicker khi icon được click
-  });
-});
+//Chức năng làm mới form
+function resetForm() {
+    document.querySelector('form').reset();
+  }
