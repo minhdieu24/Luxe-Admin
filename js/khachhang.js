cd04c67ce9d1067976d2398dbf3a21ec8536c275
pekanowf <161617512+pekanowf@users.noreply.github.com>
2024-04-10 17:17:10 +0700

    Delete js/khachhang.js
    d

--- js/khachhang.js
+++
@@ -1,6 +0,0 @@
-function ChooseNgaySinh() {
-  var ngaySinhInput = document.getElementById("NgaySinh");
-  var ngaySinh = ngaySinhInput.value;
-  console.log("Ngày sinh: " + ngaySinh);
-}
-document.getElementById("NgaySinh").addEventListener("change", ChooseNgaySinh);
